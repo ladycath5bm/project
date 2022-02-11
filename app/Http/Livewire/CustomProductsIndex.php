d86f5bcbb1866e0a58ece80ba57c0a0adeca3049
ladycath5bm <leidycath.bm@gmail.com>
2022-02-11 02:45:31 -0500

    fix: search for products ppal

---
+++ app/Http/Livewire/CustomProductsIndex.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Product;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+class CustomProductsIndex extends Component
+{
+    use WithPagination;
+
+    public $search; 
+
+    public function updatingSearch(){
+        $this->resetPage();
+    }
+
+    public function render()
+    {
+        $products = Product::where('status', true)->latest('id')->paginate(9);
+
+        return view('livewire.custom-products-index', compact('products'));
+    }
+}
