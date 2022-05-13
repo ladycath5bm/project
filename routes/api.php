63c51484ca6036ecb2a9183e223dc902a80f3f24
ladycath5bm <leidycath.bm@gmail.com>
2022-05-13 10:14:36 -0500

    style: 💄 styling routes api

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,13 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\ProductController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::apiResource('products', ProductController::class)->names('api.products');
+
+Route::post('register', [AuthController::class, 'register'])->name('api.users.register');
+Route::post('login', [AuthController::class, 'login'])->name('api.users.login');
+Route::middleware('auth:sanctum')
+    ->post('logout', [AuthController::class, 'logout'])
+    ->name('api.users.logout');
